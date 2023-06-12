8f6e52f4c4c5d927fe4af26c267a1454334607fb
zhuuchaang <3200104937@zju.edu.cn>
2023-06-12 19:19:24 +0800

    feat: clike parameters ok

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,37 @@ extern int yyparse();
 extern Program* Root;
 
 int main(int argc, const char* argv[]){
-    yyin = fopen(argv[2], "r");
+    bool print_ast=false;
+    std::string optlevel;
+    if(argc==1){
+        yyin=fopen(argv[1], "r");
+    }else if(argc==2){
+        std::string s1(argv[1]);
+        if(s1=="-p"){
+            print_ast=true;
+        }else if(s1.find("-O")==0){
+            optlevel=s1;
+        }
+        yyin = fopen(argv[2], "r");
+    }else if(argc==3){
+        std::string s1(argv[1]);
+        std::string s2(argv[2]);
+        if(s1=="-p"){
+            print_ast=true;
+        }
+        if(s2.find("-O")==0){
+            optlevel=s2;
+        }
+        yyin = fopen(argv[2], "r");
+    }else{
+        std::cout<<"wrong number of value"<<std::endl;
+    }
     assert(yyin);
     yyparse();
-    Root->DrawNode(0);
-    CodeGenerator Gen("");
+    if(print_ast){
+        Root->DrawNode(0);
+    }
+    CodeGenerator Gen(optlevel);
     Gen.CodeGenerate(*Root);
     Gen.OutputIRcode();
     Gen.ObjGenerate();
